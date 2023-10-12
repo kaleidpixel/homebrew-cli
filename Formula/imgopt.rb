bc5ca374cb1b29df02e6a3a0366b087691bea54c
KAZUKI Otsuhata <7222225+k4zuki02h4t4@users.noreply.github.com>
2023-10-12 10:22:58 +0900

    change of version info.

--- Formula/imgopt.rb
+++ Formula/imgopt.rb
@@ -1,8 +1,7 @@
 class Imgopt < Formula
   desc "Command-line imgopt utility"
   homepage "https://github.com/kaleidpixel/imgopt"
-  url "https://github.com/kaleidpixel/imgopt/releases/download/latest/imgopt.phar"
-  version "0.3.2"
+  url "https://github.com/kaleidpixel/imgopt/releases/download/v0.3.2/imgopt.phar"
   sha256 "AB6D4370CA291026E307226EAF541C3614FEF85BECD54EEB4D1B4969415963CC"
 
   def install
