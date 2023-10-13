3535d153e6eb68af9410dc11f73f820b1caf71d4
KAZUKI Otsuhata <7222225+k4zuki02h4t4@users.noreply.github.com>
2023-10-13 23:40:22 +0900

    fixed syntax error.

--- Formula/imgopt.rb
+++ Formula/imgopt.rb
@@ -1,7 +1,7 @@
 class Imgopt < Formula
   desc "Command-line interface for kaleidpixel/image-optimizer"
   homepage "https://github.com/kaleidpixel/imgopt"
-  https://github.com/kaleidpixel/imgopt/releases/download/v0.3.2/imgopt.phar
+  url "https://github.com/kaleidpixel/imgopt/releases/download/v0.3.2/imgopt.phar"
   sha256 "ebce8a68e0cc62639f5c6de25a0b30a05b90f44cb8bd2b508b3631b86bbfe5bd"
 
   def install
