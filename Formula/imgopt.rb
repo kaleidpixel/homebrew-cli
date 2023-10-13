4a11f2cba2bb2c7cc2f560d705ef349f881bcadc
KAZUKI Otsuhata <7222225+k4zuki02h4t4@users.noreply.github.com>
2023-10-13 18:45:49 +0900

    Updated the hash value due to a rebuild with GitHub Actions.

--- Formula/imgopt.rb
+++ Formula/imgopt.rb
@@ -1,8 +1,8 @@
 class Imgopt < Formula
-  desc "Command-line imgopt utility"
+  desc "Command-line interface for kaleidpixel/image-optimizer"
   homepage "https://github.com/kaleidpixel/imgopt"
   url "https://github.com/kaleidpixel/imgopt/releases/download/v0.3.2/imgopt.phar"
-  sha256 "AB6D4370CA291026E307226EAF541C3614FEF85BECD54EEB4D1B4969415963CC"
+  sha256 "3911B1BE69C9F4B5BBF420CBB299741C07070E066F011B3CA3B56D6FC888EC6D"
 
   def install
     bin.install "imgopt.phar" => "imgopt"
